4c76524d14b7b07b2c98a6fc3c108b79625dd052
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2023-12-21 05:38:38 +0530

    Update

--- src/browser.py
+++ src/browser.py
@@ -78,7 +78,7 @@ class Browser:
             }
 
         driver = webdriver.Chrome(
-            driver_executable_path = "/app/chromedriver",
+            driver_executable_path = "chromedriver",
             options=options,
             seleniumwire_options=seleniumwireOptions,
             user_data_dir=self.userDataDir.as_posix(),
