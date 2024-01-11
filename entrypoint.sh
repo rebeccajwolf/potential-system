cfad6c6ee2d66ff5da0b00988b807ccf59b0e8ac
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2024-01-11 21:20:39 +0530

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sh ./clean_mem.sh ;
-exec gunicorn keep_alive:app --bind 0.0.0.0:8080 &
+nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 &
 python3 main.py --everyday --visible --virtual-display;
